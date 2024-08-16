64583544f54635b4c49afdec136ed13b2eb7aed0
Hendricksoncala <156349949+Hendricksoncala@users.noreply.github.com>
2024-08-15 23:31:14 -0500

    feat: :sparkles: se crearon las carpetas router y conecciones entre los archivos

--- server/controller/clienteController.js
+++ server/controller/clienteController.js
@@ -1,3 +1,15 @@
 const { validationResult } = require('express-validator');
 const ClienteDTO = require('../dto/clienteDto'); // Asegúrate de que la ruta sea correcta
 const Cliente = require('../models/clienteModel'); 
+
+const ClienteManager = require('../models/clienteModel');
+
+exports.crearCliente = async (req, res) => {
+  try {
+    const nuevoCliente = req.body; // Obtén los datos del cliente de la solicitud
+    const clienteCreado = await ClienteManager.create(nuevoCliente);
+    res.status(201).json(clienteCreado); 
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
---
+++ server/router.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const router = express.Router();
+const clienteController = require('../controllers/clienteController');   
+
+
+router.post('/', clienteController.crearCliente);   
+
+// ... otras rutas para clientes
+
+module.exports = router;
